f66b1ae99262f1e8d7d543a3b413dec31aec745b
Edgar Ceron <edgar.ceron@correounivalle.edu.co>
2022-04-18 18:00:54 -0500

    Added relations to US template

--- diagrams/business_logic/transform_xml.py
+++ diagrams/business_logic/transform_xml.py
@@ -1,7 +1,4 @@
-import os
-import xmltodict
-import json
-import platform
+import os, xmltodict, json, platform
 from typing import OrderedDict, Tuple
 from xhtml2pdf import pisa
 from django.template import loader
@@ -30,10 +27,6 @@ def get_participants(diagram: dict) -> list:
     except KeyError:
         return []
 
-def get_processes(diagram: dict) -> list[dict]:
-    processes = diagram['bpmn:definitions']['bpmn:process']
-    processes = processes if isinstance(processes, list) else [processes]
-    return processes
 
 def get_participant_activities(diagram: dict, process_ref: str) -> list:
     processes = get_processes(diagram)
@@ -43,8 +36,62 @@ def get_participant_activities(diagram: dict, process_ref: str) -> list:
             return tasks if isinstance(tasks, list) else [tasks]
     return []
 
+def add_depend(id: str, x, dependencies: dict):
+    if id in dependencies: dependencies[id].append(x)
+    else: dependencies[id] = [x]
 
-def get_activity_names(diagram: dict):
+def get_dependencies(diagram: dict):
+    dependencies = {}
+    processes = get_processes(diagram)
+    for i in processes:
+        keys = i.keys()
+        for j in keys:
+            ele = i[j] if isinstance(i[j], list) else [i[j]]
+            for m in ele:
+                if '@sourceRef' in m:
+                    add_depend(m['@id'], m['@sourceRef'], dependencies)
+                if 'bpmn:incoming' in m:
+                    l = m['bpmn:incoming']
+                    l = l if isinstance(l, list) else [l]
+                    for k in l:
+                        add_depend(m['@id'], k, dependencies)
+    get_collaboration_flows(diagram, dependencies)
+    return dependencies
+
+def get_collaboration_flows(diagram: dict, dependencies: dict):
+    if 'bpmn:messageFlow' in diagram['bpmn:definitions']['bpmn:collaboration']:
+        flows = diagram['bpmn:definitions']['bpmn:collaboration']['bpmn:messageFlow']
+        for m in flows:
+            if '@sourceRef' in m:
+                add_depend(m['@id'], m['@sourceRef'], dependencies)
+
+def get_activity_relations(activity: dict, dependencies: dict):
+    relations = []
+    if 'bpmn:incoming' in activity:
+        incoming = activity['bpmn:incoming']
+        incoming = incoming if isinstance(incoming, list) else [incoming]
+        for i in incoming:
+            relations += get_last_activities(i, dependencies)
+    return relations
+
+def get_relations_with_names(relations: list, names: dict):
+    relations = [i + ' ' + names[i] for i in relations]
+    return relations
+
+def get_last_activities(id: str, dependencies: dict):
+    if id == '': return []
+    if id.startswith('Activity'): return [id]
+    relations = []
+    for i in dependencies.get(id, []):
+        relations = relations + get_last_activities(i, dependencies)
+    return relations
+
+def get_processes(diagram: dict) -> list[dict]:
+    processes = diagram['bpmn:definitions']['bpmn:process']
+    processes = processes if isinstance(processes, list) else [processes] 
+    return processes
+
+def get_activity_names(diagram: dict) -> dict:
     names = {}
     processes = get_processes(diagram)
     for i in processes:
@@ -54,41 +101,7 @@ def get_activity_names(diagram: dict):
             names[j['@id']] = j['@name']
     return names
 
-
-def get_flows(diagram: dict):
-    sources = {}
-    processes = get_processes(diagram)
-    for i in processes:
-        keys = i.keys()
-        for j in keys:
-            parts = i.get(j, [])
-            parts = parts if isinstance(parts, list) else [parts]
-            for k in parts:
-                if not isinstance(k, str):
-                    sources[k['@id']] = k.get('@sourceRef', '')
-    flows = diagram['bpmn:definitions']['bpmn:collaboration'].get('bpmn:messageFlow', [])
-    flows = flows if isinstance(flows, list) else [flows]
-    for i in flows:
-        sources[i['@id']] = i['@sourceRef']
-    return sources
-
-def get_last_process(id: str, sources: dict):
-    while id != '' and not id.startswith('Activity'):
-        id = sources.get(id, '')
-    return id
-
-
-def get_incoming_activities(activity: dict, names: dict, sources: dict):
-    incoming = activity.get('bpmn:incoming', [])
-    incoming_items = incoming if isinstance(incoming, list) else [incoming]
-    incoming_ids = [get_last_process(sources[x], sources) for x in incoming_items]
-    print(activity['@id'])
-    incoming_task = [x + " -> " + names[x] for x in incoming_ids if x in names]
-    incoming_task = '\n'.join(incoming_task)
-    return incoming_task
-
-
-def get_data_for_us(diagram_id: str):
+def get_data_for_us(diagram_id):
     diagram_data = get_diagram(diagram_id)
     data = []
     if diagram_data is None:
@@ -97,15 +110,15 @@ def get_data_for_us(diagram_id: str):
     props = json.loads(diagram_data.propierties)
     diagram = transform_to_obj(xml)
     participants = get_participants(diagram)
+    dependencies = get_dependencies(diagram)
     names = get_activity_names(diagram)
-    flows = get_flows(diagram)
     for role in participants:
         name = role.get('@name', '')
         tasks = get_participant_activities(diagram, role['@processRef'])
         for i in tasks:
             task_id = i['@id']
-            incoming = get_incoming_activities(i, names, flows)
-            print(incoming)
+            relations =  get_activity_relations(i, dependencies)
+            relations = get_relations_with_names(relations, names)
             data.append({
                 'id': task_id,
                 'project': diagram_data.project.name,
@@ -116,26 +129,25 @@ def get_data_for_us(diagram_id: str):
                 'criteria':  props[task_id].get('criteria', ''),
                 'points':  props[task_id].get('points', ''),
                 'restrictionsw':  props[task_id].get('restrictions', ''),
+                'relations': relations
             })
     return data
 
-
-def has_participants(diagram_id):
+def has_participants(diagram_id: int):
     diagram_data = get_diagram(diagram_id)
     xml = diagram_data.xml
     diagram = transform_to_obj(xml)
     participants = get_participants(diagram)
     return (create_diagram_us(diagram_id) if len(participants) else Response(
-        {"message": "Recuerde añadir un carril para representar el actor responsable del proceso"},
+        { "message": "Recuerde añadir un carril para representar el actor responsable del proceso" },
         status=status.HTTP_200_OK,
         content_type='application/json'
     ))
 
-
 def generate_us(data: dict) -> Tuple[bool, str]:
     source_html = loader.render_to_string('diagrams/ustemplate.html', data)
     path = settings.STATIC_ROOT
-    file_name = os.path.join(path, data['project'], data['id'] + '.pdf')
+    file_name = os.path.join(path, data['project'] , data['id'] + '.pdf')
     if not os.path.exists(os.path.dirname(file_name)):
         os.makedirs(os.path.dirname(file_name))
     result_file = open(file_name, "w+b")
@@ -143,17 +155,15 @@ def generate_us(data: dict) -> Tuple[bool, str]:
     result_file.close()
     return pisa_status.err, file_name
 
-
-def add_files_to_zip(files, diagram_id):
+def add_files_to_zip(files: list, diagram_id: str):
     path = settings.STATIC_ROOT
-    file_name = os.path.join(path, 'usfiles', diagram_id + '.zip')
+    file_name = os.path.join(path, 'usfiles' , diagram_id + '.zip')
     zip_obj = ZipFile(file_name, 'w')
     for i in files:
         zip_obj.write(i)
     zip_obj.close()
 
-
-def create_diagram_us(diagram_id):
+def create_diagram_us(diagram_id: int):
     data = get_data_for_us(diagram_id)
     files = []
     for us in data:
@@ -161,23 +171,23 @@ def create_diagram_us(diagram_id):
         print(file_name)
         if not error:
             files.append(file_name)
-        else:
+        else: 
             print(error)
 
     if data == []:
         message = "No se encontraron procesos para el diagrama escogido"
         status_code = status.HTTP_200_OK
     else:
-        message = "Se generaron {} historia/s para el diagrama escogido".format(
-            str(len(data)))
+        message = "Se generaron {} historia/s para el diagrama escogido".format(str(len(data)))
         status_code = status.HTTP_200_OK
 
+    
     answer = {
         "message": message
     }
 
     return Response(
-        answer,
-        status=status_code,
-        content_type='application/json'
+            answer,
+            status=status_code,
+            content_type='application/json'
     )
